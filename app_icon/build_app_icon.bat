aeafa012b7c54d33d058c42371fa66bd8a79bec4
WC Leung <lwchkg@gmail.com>
2017-05-31 01:03:12 +0800

    Add app icon with 48px image (#10)
    * Add app icon with 48px image
    * Added: 48px SVG icon
    * Added: SVG -> PNG -> ICO building script
      (requires Inkscape and ImageMagick)
    * Built DotNetEditor.ico

---
+++ app_icon/build_app_icon.bat
@@ -0,0 +1,20 @@
+@echo off
+rem Requires Inkscape and ImageMagick installed in default path
+
+if "%1"=="clean" goto clean
+setlocal enableDelayedExpansion
+set icolist=
+set inkscape_path=%ProgramFiles%\Inkscape\
+for %%k in (*.svg) do (
+  "%inkscape_path%inkscape" -f "%%k" -e "%%~nk.png"
+  set icolist=!icolist! "%%~nk.png"
+)
+
+magick%icolist% ..\DotNetEditor\DotNetEditor.ico
+endlocal
+goto end
+
+:clean
+del *.png /q
+
+:end
