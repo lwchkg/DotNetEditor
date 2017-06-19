95338bd6dd89bff22614cccc93bcd5b9775a42b4
WC Leung <lwchkg@gmail.com>
2017-06-19 09:56:25 +0000

    Extra resolutions for the app icon, update icon building script (#11)
    - Add 16px, 24px, 32px, 40px, 64px and 256px icons.
    - Update the .ico file build script
    - Update .gitattributes to treat *.ico as binary

--- app_icon/build_app_icon.bat
+++ app_icon/build_app_icon.bat
@@ -1,20 +1,46 @@
 @echo off
-rem Requires Inkscape and ImageMagick installed in default path
-
+if "%1"=="build" goto build
 if "%1"=="clean" goto clean
+
+echo ICO file builder
+echo ================
+echo Builds the target ICO file with every SVG file in this directory. Requires
+echo Inkscape and ImageMagick to be installed before running.
+echo+
+echo Usage:
+echo     %~n0 build - builds the ICO file (and intermediate PNG files)
+echo     %~n0 clean - deletes the intermediate PNG files
+echo+
+
+choice /m "Continue to build the ICO file"
+if errorlevel 2 goto end
+echo+
+
+:build
+echo Creating PNG files from SVGs...
+echo+
+echo Note: Inkscape may fail to exit after creating the PNG file. In this case,
+echo terminate Inkscape in the Task Manager to continue execution.
+echo+
 setlocal enableDelayedExpansion
+set target=..\DotNetEditor\DotNetEditor.ico
+
 set icolist=
 set inkscape_path=%ProgramFiles%\Inkscape\
 for %%k in (*.svg) do (
-  "%inkscape_path%inkscape" -f "%%k" -e "%%~nk.png"
+  echo Processing %%k...
+  "%inkscape_path%inkscape" -C -f "%%k" -e "%%~nk.png">nul
   set icolist=!icolist! "%%~nk.png"
 )
 
-magick%icolist% ..\DotNetEditor\DotNetEditor.ico
+echo+
+echo PNG creation finished. Now creating ICO file from PNGs...
+magick%icolist% "%target%"
 endlocal
 goto end
 
 :clean
+echo Removing PNG files...
 del *.png /q
 
 :end
